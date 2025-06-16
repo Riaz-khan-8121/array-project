ea5a3ff299b5459fa7ae2ab518bb2ccfed2cb416
Riaz-khan-8121 <riazkakar8121940@gmail.com>
2025-06-16 21:53:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,144 @@
+// 1. Create an array of 3 student names and add one more using push().
+
+// let students=['Riaz','khan','kakar'];
+
+// let add=students.push('abdul');
+// console.log(students);
+
+
+
+// 2. Remove the last item from an array of fruits using pop().
+
+// let fruits=['apple','banana','cherry'];
+// let remove=fruits.pop();
+// console.log(fruits);
+
+
+
+// 3. Add "Monday" to the beginning of a days array using unshift().
+// let days=['Tuesday','Wednesday','Thursday'];
+// let add=days.unshift('Monday');
+// console.log(days);
+
+
+
+// 4. Remove the first item from an array of colors using shift().
+// let colors=['red','green','blue'];
+// let remove=colors.shift();
+// console.log(colors);
+
+
+
+// 5. Use splice() to insert "Math" and "Science" at index 2 of a subjects array.
+// let subjects=['English','History','Geography'];
+// let add=subjects.splice(2,0,'Math','Science');
+// console.log(subjects);
+
+
+
+// 6. Use splice() to remove 2 elements starting from index 1 of a cities array.
+// let cities=['Karachi','Lahore','Islamabad','Faisalabad'];
+// let remove=cities.splice(1,2);
+// console.log(cities);
+
+
+
+// 7. Use slice() to get the first 3 items from a countries array.
+// let countries=['Pakistan','India','China','USA'];
+// let firstThree=countries.slice(0,3);
+// console.log(firstThree);
+
+
+
+// 8. Convert the string "apple,banana,mango" into an array using split().
+// let fruits='apple,banana,mango';
+// let array=fruits.split(',');
+// console.log(array);
+
+
+// 9. Join an array of characters ['J','S'] into a single string using join().
+// let characters=['J','S'];
+// let string=characters.join('');
+// console.log(string);
+
+
+
+// 10. Combine two arrays ['pen','pencil'] and ['eraser','sharpener'] using concat().
+// let tools=['pen','pencil'];
+// let tool=['eraser','sharpener'];
+// let combined=tools.concat(tool);
+// console.log(combined);
+
+
+
+// 12. Sort an array of names alphabetically using sort().
+// let names=['Ali','Ahmed','Sana','Saba'];
+// let sor=names.sort();
+// console.log(sor)
+// console.log(names);
+
+
+
+// 13. Reverse the order of an array of numbers using reverse().
+// let numbers=[1,2,3,4,5];
+// let reversed=numbers.reverse();
+// console.log(reversed);
+// console.log(numbers);
+
+
+
+// 14. Use indexOf() to find the index of "Blue" in a colors array.
+// let colors=['Red','Green','Blue','Yellow'];
+// let index=colors.indexOf('Blue');    
+// console.log(index);
+// console.log(colors[index]);
+
+
+
+// 15. Add three new items to an empty array using push().
+// let emptyArray=[];
+// emptyArray.push('apple');
+// emptyArray.push('banana');
+// emptyArray.push('mango');
+// console.log(emptyArray);
+
+
+
+// 16. Remove the middle item from an array of 5 numbers using splice().
+// let numbers=[1,2,3,4,5];
+// let remove=numbers.splice(2,1);
+// console.log(remove);
+// console.log(numbers);
+
+
+
+// 17. Create a string "HTML CSS JavaScript" and convert it to an array using split().
+// let str="HTML CSS JavaScript";
+// let array=str.split(' ');
+// console.log(array);
+
+
+
+
+// 18. Create an array ['a', 'b', 'c'], reverse it, then join it with - in between.
+// let array=['a','b','c'];
+// let reverse=array.reverse();
+// let join=reverse.join('-');
+// console.log(join);
+
+
+
+// 19. Check if "Karachi" exists in an array using indexOf() and show its index.
+// let cities=['Lahore','Karachi','Islamabad'];
+// let index=cities.indexOf('Karachi');
+// console.log(index);
+
+
+
+// 20. Concatenate two arrays of even and odd numbers using concat() and then reverse them
+// let even=[2,4,6,8];
+// let odd=[1,3,5,7];
+// let con=even.concat(odd);
+// let reversed=con.reverse();
+// console.log(reversed);
+
